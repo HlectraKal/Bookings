b5f38d71ef4959c3f61e3c8bdc0bdc22003991fe
HlectraKal <hlectra16@gmail.com>
2024-12-01 14:03:16 +0200

    Add files via upload

---
+++ Car Insurance US.sql
@@ -0,0 +1,33 @@
+use insurancecar
+
+select *
+from insurance;
+
+-- Η 2η ΠΟΛΙΤΕΙΑ ΜΕ ΤΟ ΜΕΓΑΛΥΤΕΡΟ ΚΟΣΤΟΣ ΜΙΚΤΗΣ ΑΣΦΑΛΙΣΗΣ
+
+select state, full_coverage
+from insurance
+order by full_coverage desc
+limit 1
+offset 1;
+
+-- Η 2η ΜΙΚΡΟΤΕΡΗ ΔΙΑΦΟΡΑ ΑΣΦΑΛΙΣΗΣ ΜΕΤΑΞΥ ΜΙΚΤΗΣ ΚΑΙ ΒΑΣΙΚΗΣ 
+
+select min(difference) as min_dif
+from insurance
+where difference not in
+(
+  select min(difference)
+  from insurance
+);
+
+-- ΟΙ 5 ΠΡΩΤΕΣ ΠΟΛΙΤΕΙΕΣ ΠΟΥ ΕΧΟΥΝ ΜΙΚΤΗ ΑΣΦΑΛΙΣΗ ΠΑΝΩ ΑΠΟ 1000 ΚΟΣΤΟΣ
+
+select state, full_coverage
+from insurance
+where full_coverage > 1000
+order by full_coverage desc
+limit 5;
+
+
+
